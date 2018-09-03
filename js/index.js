009d123e1c2931e3b331a0f16116ae00c82da59c
abhay maurya <jya9838@gmail.com>
2018-09-03 07:01:44 +0530

    basic components added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
-//this space for js
+$( document ).ready(function() {
 
 
-var dsf=54
+var rate = 4.5;
+var starTotal = 5.0;
+const starPercentage = (rate / starTotal) * 100;
+const starPercentageRounded = Math.round(starPercentage / 10) * 10+"%";
+console.log(starPercentageRounded);
+document.querySelector('.stars-inner').style.width = starPercentageRounded;
 
-name();
 
 
-function name(){
-    console.log("hello")
-}
+var countDownDate = new Date().getTime();
+
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = now - countDownDate ;
+    //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("demo").innerHTML = minutes + ":" +seconds;
+}, 1000);
+
+
+
+
+
+});
 
